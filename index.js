c9c15baf0fbbfba90fc04239060a60684132eaf3
Shola Nur <shola.nur@gmail.com>
2023-03-27 17:03:19 -0700

    drum js project

---
+++ index.js
@@ -0,0 +1,10 @@
+let button = document.querySelectorAll(".drum");
+
+let numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i < numberOfDrumButtons; i++) {
+  button[i].addEventListener("click", () => {});
+}
+
+//let audio = new Audio("./sounds/tom-1.mp3");
+// audio.play();

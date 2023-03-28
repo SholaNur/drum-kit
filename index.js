bd9795a915cd8387612e75beeeb23da708e914e7
Shola Nur <shola.nur@gmail.com>
2023-03-28 13:59:15 -0700

    key event function and sounds added

--- index.js
+++ index.js
@@ -1,10 +1,50 @@
-let button = document.querySelectorAll(".drum");
-
+// Here sounds will play on button click.
 let numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (let i = 0; i < numberOfDrumButtons; i++) {
-  button[i].addEventListener("click", () => {});
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    let buttonInnerHtml = this.innerHTML;
+    makeSound(buttonInnerHtml);
+  });
 }
 
-//let audio = new Audio("./sounds/tom-1.mp3");
-// audio.play();
+// Here sounds play when key is pressed.
+document.addEventListener("keydown", function (event) {
+  makeSound(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      let crash = new Audio("./sounds/crash.mp3");
+      crash.play();
+      break;
+    case "a":
+      let kickBass = new Audio("./sounds/kick-bass.mp3");
+      kickBass.play();
+      break;
+    case "s":
+      let snare = new Audio("./sounds/snare.mp3");
+      snare.play();
+      break;
+    case "d":
+      let tom1 = new Audio("./sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "j":
+      let tom2 = new Audio("./sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "k":
+      let tom3 = new Audio("./sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "l":
+      let tom4 = new Audio("./sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    default:
+      console.log(buttonInnerHtml);
+  }
+}
